55b01e6c880fba12bba1a33271bdcfe87dd03f5d
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-05-25 14:03:30 -0400

    pulling a single repo with handlebars and styling it

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 import { token } from './access_token.js';
 
-fetch(`https://api.github.com/users/willbraun`, {
+const generateHTML = data => {
+    const source = document.querySelector('#repo-template').innerHTML;
+    const template = Handlebars.compile(source);
+    const html = template(data);
+    document.querySelector('.repos').innerHTML = html;
+}
+
+fetch(`https://api.github.com/users/willbraun/repos`, {
     headers: {
         Authorization: `token ${token}`,
     },
     })
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+        generateHTML(data[0]);
+        console.log(data[0]);
+    });
 
-    
