8e4a75d040b05433cb7caa2af50620526f9df4eb
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-05-25 21:41:03 -0400

    test

--- js/main.js
+++ js/main.js
@@ -37,4 +37,4 @@ fetchAndDo(`https://api.github.com/users/willbraun/orgs`, orgArray => {
        
 
 console.log(format(new Date(2014, 1, 11), 'MM/dd/yyyy'));
-    //=> '02/11/2014' f
+    //=> '02/11/2014' 
