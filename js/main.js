4b12c5c55bd6ed523dba0c469e9d5bf295742856
Will Braun <willbraun@Wills-MacBook-Pro.local>
2022-05-25 23:46:22 -0400

    added function to calculate elapsed time using date fns cdn and used handlebars to insert into the repo template

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { token } from './access_token.js';
-// import format from './../node_modules/date-fns/format/index.js';
+// import addDays from './../node_modules/date-fns/addDays/index.js';
 
 const generateRepoHTML = data => {
     const source = document.querySelector('#repo-template').innerHTML;
@@ -7,6 +7,25 @@ const generateRepoHTML = data => {
     const html = template(data);
     document.querySelector('.repos').innerHTML = html;
 }
+
+Handlebars.registerHelper('findDaysElapsed', function (startDate) {
+    const now = new Date();
+    const date = new Date(startDate);
+
+    const seconds = dateFns.differenceInSeconds(now, date);
+    if (seconds < 60) {return `Updated less than 1 minute ago`};
+
+    const minutes = dateFns.differenceInMinutes(now, date);
+    if (minutes < 60) {return `Updated ${minutes} minutes ago`};
+
+    const hours = dateFns.differenceInHours(now, date);
+    if (hours < 24) {return `Updated ${hours} hours ago`};
+    
+    const days = dateFns.differenceInDays(now, date);
+    if (days === 1) {return `Updated 1 day ago`}
+    else if (days < 32) {return `Updated ${days} days ago`}
+    else {return `Updated on ${dateFns.format(date, 'MMM d, YYYY')}`};
+})
 
 const fetchAndDo = (url, callback) => {
     fetch(url, {
@@ -21,20 +40,15 @@ const fetchAndDo = (url, callback) => {
 fetchAndDo(`https://api.github.com/users/willbraun/repos`, repoArray => {
     generateRepoHTML({repos: repoArray});
     document.querySelector('.count').innerHTML = repoArray.length.toString();
-    console.log(repoArray);
+
 })
 
 fetchAndDo(`https://api.github.com/users/willbraun`, personalData => {
     document.querySelector('.profile-pic').src = personalData.avatar_url;
     document.querySelector('.profile-pic').alt = personalData.name;
-    console.log(personalData);
 })
 
 fetchAndDo(`https://api.github.com/users/willbraun/orgs`, orgArray => {
     document.querySelector('.org-pic').src = orgArray[0].avatar_url;
     document.querySelector('.org-pic').alt = orgArray[0].login;
 })
-       
-
-console.log(format(new Date(2014, 1, 11), 'MM/dd/yyyy'));
-    //=> '02/11/2014' 
